ce0e763ba1eeb975a05ca91837f06aebd008a104
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-01-21 03:13:39 -0800

    Finished 0.9 Update

--- Daybreak+/Daybreak.cs
+++ Daybreak+/Daybreak.cs
@@ -6,108 +6,61 @@ using System.Threading.Tasks;
 using BlockEntities.Implementations;
 using ModLoaderInterfaces;
 using Pipliz;
+using static BlockEntities.Implementations.BannerTracker;
 
 namespace Daybreak_
 {
     public class Daybreak : IOnUpdate
     {
-        public const string NAMESPACE = "ModEntry";
-        public static string MOD_DIRECTORY;
-
-        public const string ASSETS = "assets";
-
         public static float lastTime = 0;
         public void OnUpdate()
         {
             float currentTime = TimeCycle.TimeOfDayHours;
 
-            // check daybreak
+            // check for daybreak/nightfall
             if (System.Math.Abs(lastTime) > 0.05)
             {
                 if (currentTime > TimeCycle.SunRise && lastTime <= TimeCycle.SunRise)
                 {
-                    //Log.Write("Playing Daybreak sounds.");
+                    Log.Write("Playing Daybreak sound at all active banners.");
 
                     for (int i = 0; i < Players.ConnectedPlayers.Count; i++)
                     {
-                        //Players.Player p = Players.GetConnectedByIndex(0);
-                        PlayDaybreakSoundForPlayer(Players.ConnectedPlayers[i], false);
+                        PlaySoundAtAllActiveBanners(Players.ConnectedPlayers[i], true);
                     }
                 }
                 if (currentTime > TimeCycle.SunSet && lastTime <= TimeCycle.SunSet)
                 {
-                    //Log.Write("Playing Nightfall sounds.");
+                    Log.Write("Playing Nightfall sound at all active banners.");
 
                     for (int i = 0; i < Players.ConnectedPlayers.Count; i++)
                     {
-                        //Players.Player p = Players.GetConnectedByIndex(0);
-                        PlayNightfallSoundForPlayer(Players.ConnectedPlayers[i], false);
+                        PlaySoundAtAllActiveBanners(Players.ConnectedPlayers[i], false);
                     }
                 }
             }
-            // check nightfall
-            //TimeCycle.SunSet;
 
             lastTime = currentTime;
         }
-        public static void PlaySoundAtBanner(BannerTracker.Banner banner, string soundname)
+
+        public void PlaySoundAtAllActiveBanners(Players.Player player, bool isDaybreakSound)
+        {
+            for (int i = 0; i < player.ActiveColonyGroup.Colonies.Count; i++)
+            {
+                for (int i2 = 0; i2 < player.ActiveColonyGroup.Colonies[i].Banners.Count; i2++)
+                {
+                    if (isDaybreakSound)
+                        PlaySoundAtBanner(player.ActiveColonyGroup.Colonies[i].Banners[i2], "daybreak");
+                    else
+                        PlaySoundAtBanner(player.ActiveColonyGroup.Colonies[i].Banners[i2], "nightfall");
+                }
+            }
+        }
+
+        public void PlaySoundAtBanner(BannerTracker.Banner banner, string soundname)
         {
             UnityEngine.Vector3 bannerpos = new UnityEngine.Vector3(banner.Position.x, banner.Position.y, banner.Position.z);
             AudioManager.SendAudio(bannerpos, soundname);
-        }
-
-        public static void PlayDaybreakSoundForPlayer(Players.Player player, bool OnlyActiveColony)
-        {
-            if (OnlyActiveColony)
-            {
-                try
-                {
-                    BannerTracker.Banner b = player.ActiveColony.Banners[0];
-                    PlaySoundAtBanner(b, "daybreak");
-
-                }
-                catch (System.Exception)
-                {
-                    Log.Write("Couldn't play daybreak sound no active colony found");
-                }
-            }
-            else
-            {
-                List<BannerTracker.Banner> bannerlist = new List<BannerTracker.Banner>();
-
-                ServerManager.BlockEntityTracker.BannerTracker.Foreach((Vector3Int position, BannerTracker.Banner typeInstance, ref List<BannerTracker.Banner> data) => data.Add(typeInstance), ref bannerlist);
-
-                foreach (var banner in bannerlist)
-                {
-                    PlaySoundAtBanner(banner, "daybreak");
-                }
-            }
-        }
-        public static void PlayNightfallSoundForPlayer(Players.Player player, bool OnlyActiveColony)
-        {
-            if (OnlyActiveColony)
-            {
-                try
-                {
-                    BannerTracker.Banner b = player.ActiveColony.Banners[0];
-                    PlaySoundAtBanner(b, "nightfall");
-                }
-                catch (System.Exception)
-                {
-                    Log.Write("Couldn't play nightfall sound no active colony found");
-                }
-            }
-            else
-            {
-                List<BannerTracker.Banner> bannerlist = new List<BannerTracker.Banner>();
-
-                ServerManager.BlockEntityTracker.BannerTracker.Foreach((Vector3Int position, BannerTracker.Banner typeInstance, ref List<BannerTracker.Banner> data) => data.Add(typeInstance), ref bannerlist);
-
-                foreach (var banner in bannerlist)
-                {
-                    PlaySoundAtBanner(banner, "nightfall");
-                }
-            }
         }
     }
 }
